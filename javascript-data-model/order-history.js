429e954777fb074877c45b9e29c68b7fb2034d67
Seungjoo Kim <sjskybone@gmail.com>
2021-03-16 12:51:25 -0700

    Completed javascript-data-model. | Seungjoo Kim

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,86 @@
+var orderHistory = [
+  {
+    orderPlaced: 'Aug 04, 2020',
+    orderDelivered: 'Aug 08, 2020',
+    orderNumber: '114-3941689-8772232',
+    shipTo: 'JS Masher',
+    itemsInfo: [
+      {
+        product: 'book',
+        name: 'JavaScript for impatient programmers',
+        itemData: {
+          author: 'Rauschmayer, Dr. Axel'
+        },
+        price: '$31.55',
+        total: '$34.00',
+        returnDate: 'Sep 07, 2020'
+      }
+    ]
+  },
+  {
+    orderPlaced: 'Jul 19, 2020',
+    orderDelivered: 'Jul 20, 2020',
+    orderNumber: '113-9984268-1280257',
+    shipTo: 'JS Masher',
+    itemsInfo: [
+      {
+        product: 'book',
+        name: 'The Timeless Way of Building',
+        itemData: {
+          author: 'Alexander, Christopher'
+        },
+        price: '$41.33',
+        total: '$44.53',
+        returnDate: 'Aug 19, 2020'
+      }
+    ]
+  },
+  {
+    orderPlaced: 'Jul 4, 2020',
+    orderDelivered: 'Jul 7, 2020',
+    orderNumber: '114-2875557-9059409',
+    shipTo: 'JS Masher',
+    itemsInfo: [
+      {
+        product: 'controller adapter',
+        name: 'Gamecube Controller Adapter, Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        itemData: {
+          gameConsole: 'Nintendo, GameCube'
+        },
+        price: '$15.98',
+        total: '$17.22',
+        returnDate: 'Aug 5, 2020'
+      }
+    ]
+  },
+  {
+    orderPlaced: 'Jul 3, 2020',
+    orderDelivered: 'Jul 5, 2020',
+    orderNumber: '113-2883177-2648248',
+    shipTo: 'JS Masher',
+    itemsInfo: [
+      {
+        product: 'controller',
+        name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        itemData: {
+          gameConsole: 'Nintendo, GameCube'
+        },
+        price: '$94.95',
+        total: '138.93',
+        returnDate: 'Aug 4, 2020'
+      },
+      {
+        product: 'book',
+        name: 'The Art of Sql',
+        itemData: {
+          author: 'Faroult, Stephane'
+        },
+        price: '$33.99',
+        total: '138.93',
+        returnDate: 'Aug 4, 2020'
+      }
+    ]
+  }
+];
+
+console.log('value of orderHistory:', orderHistory);
