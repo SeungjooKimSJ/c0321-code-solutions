758280cc94997075d1b02182acd5e6a8f555be27
Seungjoo Kim <sjskybone@gmail.com>
2021-03-20 15:36:30 -0700

    In progress object-manipulation-1.

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,10 @@
+/* exported getValue */
+// function getValue(object, key) {
+//   var keys = [];
+
+//   // for (var key in object) {
+//   //   keys.push(object[key]);
+//   // }
+//   // return keys;
+//     // keys.push(Object.entries(key))
+// }
