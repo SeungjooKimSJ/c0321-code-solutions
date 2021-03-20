996fc38b24dff80d823706048e700f1cec827d90
Seungjoo Kim <sjskybone@gmail.com>
2021-03-20 13:14:26 -0700

    Completed array-manipulation-1. | Seungjoo Kim

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,21 +1,9 @@
 /* exported compact */
 function compact(array) {
   var newArr = [];
-  // var falseValues = [false, null, undefined, 0, -0, NaN, ""];
-  // var isNaN = function (value) {
-  //   var num = Number(value);
-
-  //   return n !== n;
-  // };
-  // function typeOfNaN(x) {
-  //   if (isNaN(x)) {
-  //     return 'NaN'
-  //   }
-  // }
-  var notANumber = Number.isNaN();
 
   for (var i = 0; i < array.length; i++) {
-    if (array[i] !== false && array[i] !== null && array[i] !== undefined && array[i] !== 0 && array[i] !== -0 && array[i] !== notANumber && array[i] !== '') {
+    if (array[i]) {
       newArr.push(array[i]);
     }
   }
