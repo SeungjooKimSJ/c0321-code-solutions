350960744d478bdd127cbad1221aa0f8ab17e0af
Seungjoo Kim <sjskybone@gmail.com>
2021-03-22 18:38:54 -0700

    Completed object-manipulation-1.

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,4 +1,10 @@
 /* exported toObject */
-// function toObject(keyValuePair) {
+function toObject(keyValuePair) {
+  var newObj = {};
+  var property = keyValuePair[0];
+  var value = keyValuePair[1];
 
-// }
+  newObj[property] = value;
+
+  return newObj;
+}
