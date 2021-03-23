cc0d00e2bf0c6fa77be6aff35f08792f2c438d5b
Seungjoo Kim <sjskybone@gmail.com>
2021-03-23 16:10:33 -0700

    Delete commented out code.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -6,11 +6,11 @@ $bulb.addEventListener('click', onAndOff);
 
 function onAndOff(event) {
   if (isOn) {
-    $background.className = 'background light';
-    $bulb.className = 'bulb on';
-  } else {
     $background.className = 'background dark';
     $bulb.className = 'bulb off';
+  } else {
+    $background.className = 'background light';
+    $bulb.className = 'bulb on';
   }
   isOn = !isOn;
 }
