cba5e782e16ee9c88c38d4bdafcd9730581b9c44
Seungjoo Kim <sjskybone@gmail.com>
2021-03-23 22:52:37 -0700

    Completed javascript-modal. | Seungjoo Kim

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,16 +1,18 @@
-// var $openModalBtn = document.querySelector('.open-modal-btn');
-// var $modalContainer = document.querySelector('.modal-container');
-// var $modalContent = document.querySelector('.modal-content');
-// var $closeModalBtn = document.querySelector('.close-modal-btn');
+var $openModalBtn = document.querySelector('.open-modal-btn');
+var $modalContainer = document.querySelector('.modal-container');
+var $modalContent = document.querySelector('.modal-content');
+var $closeModalBtn = document.querySelector('.close-modal-btn');
 
-// $openModalBtn.addEventListener('click', openModal);
+$openModalBtn.addEventListener('click', openModal);
 
-// $closeModalBtn.addEventListener('click', closeModal);
+$closeModalBtn.addEventListener('click', closeModal);
 
-// function openModal(event) {
+function openModal(event) {
+  $modalContainer.className = 'modal-container off';
+  $modalContent.className = 'modal-content';
+}
 
-// }
-
-// function closeModal(event) {
-
-// }
+function closeModal(event) {
+  $modalContainer.className = 'modal-container';
+  $modalContent.className = 'modal-content hidden';
+}
