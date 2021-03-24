dc0236960de92d8b2e51546a0412a2aedaa00ae7
Seungjoo Kim <sjskybone@gmail.com>
2021-03-23 17:55:26 -0700

    Added HTML and styling.

---
+++ javascript-modal/main.js
@@ -0,0 +1,16 @@
+// var $openModalBtn = document.querySelector('.open-modal-btn');
+// var $modalContainer = document.querySelector('.modal-container');
+// var $modalContent = document.querySelector('.modal-content');
+// var $closeModalBtn = document.querySelector('.close-modal-btn');
+
+// $openModalBtn.addEventListener('click', openModal);
+
+// $closeModalBtn.addEventListener('click', closeModal);
+
+// function openModal(event) {
+
+// }
+
+// function closeModal(event) {
+
+// }
