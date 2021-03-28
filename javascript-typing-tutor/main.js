89091a9cb71cba58f7d6ba3fa8e723f30645d2bd
Seungjoo Kim <sjskybone@gmail.com>
2021-03-28 12:32:53 -0700

    Added html and styles.

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,9 @@
+// var $typingQuote = document.querySelector('.typing-quote');
+
+// for (var i = 0; i < $typingQuote.length; i++) {
+//   var $span = document.createElement('span');
+
+//   $span.textContent = $typingQuote[i];
+//   $typingQuote.appendChild($span);
+// }
+// console.log($span);
