7955f30077b0a0a70a3a0acdce43d0809ad270e5
Seungjoo Kim <sjskybone@gmail.com>
2021-03-28 14:55:35 -0700

    Working on the keydown function

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -7,3 +7,20 @@ for (var i = 0; i < $stringQuote.length; i++) {
   $span.textContent = $stringQuote[i];
   $typingQuote.appendChild($span);
 }
+
+var spanIndex = 0;
+var $allSpans = document.querySelectorAll('span');
+
+document.addEventListener('keydown', quoteKeydown);
+
+function quoteKeydown({ key }) {
+  var $eachSpan = $allSpans[spanIndex];
+
+  if (key === $eachSpan.textContent) {
+    $eachSpan.className = 'correct hidden';
+    spanIndex++;
+    $eachSpan.className = 'underbar';
+  } else {
+    $eachSpan.className = 'incorrect';
+  }
+}
