c01bed2723b3b0f5b4dd3fefb00ad557b246341f
Seungjoo Kim <sjskybone@gmail.com>
2021-04-07 12:49:34 -0700

    In progress bank.js part

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,5 +1,18 @@
 /* exported Bank */
 function Bank() {
-  this.nextAccountNumber = nextAccountNumber;
-  this.accounts = accounts;
+  this.nextAccountNumber = 1;
+  this.accounts = [];
 }
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (this.accounts.balance > 0) {
+    var newAccount = new Account(this.nextAccountNumber, holder);
+
+    this.accounts.push(newAccount);
+    this.nextAccountNumber++;
+
+    return Account;
+  } else if (this.accounts.balance < 0) {
+    return null;
+  }
+};
