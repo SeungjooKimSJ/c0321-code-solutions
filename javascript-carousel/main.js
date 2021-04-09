50c11d79c7d5035cc6a0c28501c9cbac1b37d239
Seungjoo Kim <sjskybone@gmail.com>
2021-04-08 18:18:18 -0700

    Added clickDotIcon function.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,16 +1,15 @@
 var $leftArrow = document.querySelector('.arrow-left');
 var $rightArrow = document.querySelector('.arrow-right');
-var $dot = document.querySelector('.dot-icons');
+var $dots = document.querySelector('.dot-icons');
 
 var $cards = document.querySelectorAll('.card');
 
 var currentSlide = 0;
-var currentDot = 0;
 
 $leftArrow.addEventListener('click', slideLeft);
 $rightArrow.addEventListener('click', slideRight);
 
-$dot.addEventListener('click', clickDotIcon);
+$dots.addEventListener('click', clickDotIcon);
 
 setInterval(slideRight, 3000);
 
@@ -34,10 +33,90 @@ function slideRight(event) {
   $cards[currentSlide].className = 'card';
 }
 
-// function clickDotIcon(event) {
-//   if (currentDot === ) {
-//     console.log('hi');
-//   } else if (currentDot === ) {
-//     console.log('hi2');
-//   }
-// }
+function clickDotIcon(event) {
+  var $dot1 = document.querySelector('#dot-1');
+  var $dot2 = document.querySelector('#dot-2');
+  var $dot3 = document.querySelector('#dot-3');
+  var $dot4 = document.querySelector('#dot-4');
+  var $dot5 = document.querySelector('#dot-5');
+
+  if (event.target === $dot1) {
+    currentSlide = 0;
+    $cards[currentSlide].className = 'card';
+
+    $cards[1].className = 'card hidden';
+    $cards[2].className = 'card hidden';
+    $cards[3].className = 'card hidden';
+    $cards[4].className = 'card hidden';
+
+    $dot1.className = 'fas fa-circle';
+    $dot2.className = 'far fa-circle';
+    $dot3.className = 'far fa-circle';
+    $dot4.className = 'far fa-circle';
+    $dot5.className = 'far fa-circle';
+  }
+
+  if (event.target === $dot2) {
+    currentSlide = 1;
+    $cards[currentSlide].className = 'card';
+
+    $cards[0].className = 'card hidden';
+    $cards[2].className = 'card hidden';
+    $cards[3].className = 'card hidden';
+    $cards[4].className = 'card hidden';
+
+    $dot1.className = 'far fa-circle';
+    $dot2.className = 'fas fa-circle';
+    $dot3.className = 'far fa-circle';
+    $dot4.className = 'far fa-circle';
+    $dot5.className = 'far fa-circle';
+  }
+
+  if (event.target === $dot3) {
+    currentSlide = 2;
+    $cards[currentSlide].className = 'card';
+
+    $cards[0].className = 'card hidden';
+    $cards[1].className = 'card hidden';
+    $cards[3].className = 'card hidden';
+    $cards[4].className = 'card hidden';
+
+    $dot1.className = 'far fa-circle';
+    $dot2.className = 'far fa-circle';
+    $dot3.className = 'fas fa-circle';
+    $dot4.className = 'far fa-circle';
+    $dot5.className = 'far fa-circle';
+  }
+
+  if (event.target === $dot4) {
+    currentSlide = 3;
+    $cards[currentSlide].className = 'card';
+
+    $cards[0].className = 'card hidden';
+    $cards[1].className = 'card hidden';
+    $cards[2].className = 'card hidden';
+    $cards[4].className = 'card hidden';
+
+    $dot1.className = 'far fa-circle';
+    $dot2.className = 'far fa-circle';
+    $dot3.className = 'far fa-circle';
+    $dot4.className = 'fas fa-circle';
+    $dot5.className = 'far fa-circle';
+  }
+
+  if (event.target === $dot5) {
+    currentSlide = 4;
+    $cards[currentSlide].className = 'card';
+
+    $cards[0].className = 'card hidden';
+    $cards[1].className = 'card hidden';
+    $cards[2].className = 'card hidden';
+    $cards[3].className = 'card hidden';
+
+    $dot1.className = 'far fa-circle';
+    $dot2.className = 'far fa-circle';
+    $dot3.className = 'far fa-circle';
+    $dot4.className = 'far fa-circle';
+    $dot5.className = 'fas fa-circle';
+  }
+}
