f603029d8bb145217320c02440f41bd74e7c30d2
Seungjoo Kim <sjskybone@gmail.com>
2021-04-28 15:22:49 -0700

    Completed node-require. | Seungjoo Kim

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -3,9 +3,9 @@ const subtractTwoNumbers = require('./subtract');
 const multiplyTwoNumbers = require('./multiply');
 const divideTwoNumbers = require('./divide');
 
+const num1 = parseFloat(process.argv[2]);
 const operation = process.argv[3];
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[4]);
+const num2 = parseFloat(process.argv[4]);
 
 if (operation === 'plus') {
   console.log('result:', addTwoNumbers(num1, num2));
@@ -15,4 +15,6 @@ if (operation === 'plus') {
   console.log('result:', multiplyTwoNumbers(num1, num2));
 } else if (operation === 'over') {
   console.log('result:', divideTwoNumbers(num1, num2));
+} else {
+  console.log('It is invalid operation!');
 }
