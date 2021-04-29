661da7ac262cca7f0b63a84838d2ec331a88301e
Seungjoo Kim <sjskybone@gmail.com>
2021-04-29 12:41:33 -0700

    Completed node-fs-writefile. | Seungjoo Kim

--- node-fs-writefile/note-to-self.js
+++ node-fs-writefile/note-to-self.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 
 const myNote = process.argv[2];
 
-fs.writeFile('note.txt', myNote.toString(), 'utf8', err => {
+fs.writeFile('note.txt', myNote.toString() + '\n', 'utf8', err => {
   if (err) throw err;
 });
--- node-fs-writefile/write-rand.js
+++ node-fs-writefile/write-rand.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 
 const getRandomNumber = Math.random();
 
-fs.writeFile('random.txt', getRandomNumber.toString(), 'utf8', err => {
+fs.writeFile('random.txt', getRandomNumber.toString() + '\n', 'utf8', err => {
   if (err) throw err;
 });
