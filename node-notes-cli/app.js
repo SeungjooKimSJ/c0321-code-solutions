15648750289b391e7121dd6b00e51014c2651f5b
Seungjoo Kim <sjskybone@gmail.com>
2021-04-29 17:28:50 -0700

    Completed the second feature, add-a-note.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,9 +1,21 @@
 const fs = require('fs');
 const data = require('./data.json');
 
-/*User can read their notes*/
+/* User can read their notes */
 for (const key in data.notes) {
   if (process.argv[2] === 'read') {
     console.log(`${key}: ${data.notes[key]}`);
   }
+}
+
+/* User can add a note */
+if (process.argv[2] === 'create') {
+  const key = data.nextId;
+
+  data.notes[key] = process.argv[3];
+  data.nextId++;
+
+  fs.writeFile('./data.json', JSON.stringify(data, null, 2), 'utf8', err => {
+    if (err) throw err;
+  });
 }
