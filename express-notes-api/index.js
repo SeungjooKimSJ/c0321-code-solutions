afe0247b4d6568ff218c4a58eb8eacb0ed881a1c
Seungjoo Kim <sjskybone@gmail.com>
2021-05-05 13:32:25 -0700

    Finished second feature.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,5 +1,29 @@
-const fs = require('fs');
+// const fs = require('fs');
 const data = require('./data.json');
 
 const express = require('express');
 const app = express();
+
+app.get('/api/notes', (req, res) => {
+  const notesArr = [];
+
+  for (const key in data.notes) {
+    notesArr.push(data.notes[key]);
+  }
+  res.json(notesArr);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  if (req.params.id < 0) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (data.notes[req.params.id] !== undefined) {
+    res.json(data.notes[req.params.id]);
+  } else if (data.notes[req.params.id] === undefined) {
+    res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
+  }
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 3000!');
+});
