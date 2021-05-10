8bdef95c827614a8635dde31ff3109951837cab0
Seungjoo Kim <sjskybone@gmail.com>
2021-05-09 22:12:13 -0700

    Completed express-notes-api. | Seungjoo Kim

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -72,8 +72,18 @@ app.put('/api/notes/:id', (req, res) => {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (noteData.content === undefined) {
     res.status(400).json({ error: 'content is a required field' });
-  } else if (noteData.content !== undefined) {
+  } else if (noteData.content !== undefined && data.notes[req.params.id] === undefined) {
     res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
+  } else if (noteData.content !== undefined && data.notes[req.params.id] !== undefined) {
+    data.notes[req.params.id].content = noteData.content;
+
+    fs.writeFile('./data.json', JSON.stringify(data, null, 2), 'utf8', err => {
+      if (err) {
+        res.status(500).json({ error: 'an unexprected error occured' });
+      } else {
+        res.status(200).json(data.notes[req.params.id]);
+      }
+    });
   }
 });
 
