e0500b74e4001190103ca22e6d2d5780d3e24390
Seungjoo Kim <sjskybone@gmail.com>
2021-05-10 20:27:13 -0700

    Finished third feature.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -39,7 +39,7 @@ app.post('/api/grades', (req, res) => {
   if (!name || !course | !score) {
     res.status(400).json({ error: 'Must include the name, course, and score' });
   } else if (!Number.isInteger(score) || score < 1 || score > 100) {
-    res.status(400).json({ error: 'Score must be an integer number between 1 and 100' });
+    res.status(400).json({ error: 'Score must be a positive integer between 1 and 100' });
   } else {
     const sql = `
       insert into "grades" ("name", "course", "score")
@@ -54,6 +54,48 @@ app.post('/api/grades', (req, res) => {
         const grade = result.rows[0];
 
         res.status(201).json(grade);
+      })
+
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'An unexpected error occurred.' });
+      });
+  }
+});
+
+app.put('/api/grades/:gradeId', (req, res) => {
+  const gradeId = parseInt(req.params.gradeId, 10);
+  const name = req.body.name;
+  const course = req.body.course;
+  const score = parseInt(req.body.score, 10);
+
+  if (!Number.isInteger(gradeId) || gradeId <= 0) {
+    res.status(400).json({ error: 'gradeId must be a positive integer' });
+  } else if (!name || !course || !score) {
+    res.status(400).json({ error: 'Must include the name, course, and score' });
+  } else if (!Number.isInteger(score) || score < 1 || score > 100) {
+    res.status(400).json({ error: 'Score must be a positive integer between 1 and 100' });
+  } else {
+    const sql = `
+      update "grades"
+        set "name" = $1,
+            "course" = $2,
+            "score" = $3
+      where "gradeId" = $4
+      returning *
+    `;
+
+    const params = [name, course, score, gradeId];
+
+    db.query(sql, params)
+      .then(result => {
+        const grade = result.rows[0];
+
+        if (!grade) {
+          res.status(404).json({ error: `Cannot find grade with gradeId ${gradeId}` });
+        } else {
+          res.status(200).json(grade);
+        }
       })
 
       .catch(err => {
