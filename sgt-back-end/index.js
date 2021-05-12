fb7fe93118255aa59cfb63b4538b208ab16ee0d0
Seungjoo Kim <sjskybone@gmail.com>
2021-05-12 15:04:08 -0700

    Completed sgt-back-end. | Seungjoo Kim

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -105,6 +105,38 @@ app.put('/api/grades/:gradeId', (req, res) => {
   }
 });
 
+app.delete('/api/grades/:gradeId', (req, res) => {
+  const gradeId = parseInt(req.params.gradeId, 10);
+
+  if (!Number.isInteger(gradeId) || gradeId <= 0) {
+    res.status(400).json({ error: 'gradeId must be a positive integer' });
+  } else {
+    const sql = `
+      delete from "grades"
+      where "gradeId" = $1
+      returning *
+    `;
+
+    const params = [gradeId];
+
+    db.query(sql, params)
+      .then(result => {
+        const grade = result.rows[0];
+
+        if (!grade) {
+          res.status(404).json({ error: `Cannot find grade with gradeId ${gradeId}` });
+        } else {
+          res.status(200).json(grade);
+        }
+      })
+
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'An unexpected error occurred.' });
+      });
+  }
+});
+
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Listening on port 3000!');
